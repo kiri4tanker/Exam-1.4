a0e448aca8380d36beeec2139b3f8bf56cb22889
Kirill Terentiev <kiri4tankerofficial@gmail.com>
2022-03-31 00:27:25 +0200

    +admin +user

---
+++ user.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="ru">
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Личный кабинет</title>
+   <link rel="stylesheet" href="asstes/css/main.css">
+   <link rel="stylesheet" href="asstes/css/media.css">
+</head>
+<body>
+   <header class="header">
+      <div class="container">
+         <div class="header__content">
+            <a href="index.php" class="logo">
+               <img src="assets/images/logo/logo.png" alt="LOGO" class="logo__img">
+            </a>
+            <a href="login.php" class="btn">Главная</a>
+         </div>
+      </div>
+   </header>
+   <main class="main">
+      <section class="section">
+         <div class="container">
+            <div class="section__heading">
+               <h1 class="section__title">Личный кабинет</h1>
+            </div>
+            <div class="section__content">
+               
+            </div>
+         </div>
+      </section>
+   </main>
+   <footer class="footer">
+      <div class="container">
+         <div class="footer__content">
+            <a href="index.php" class="logo">
+               <img src="assets/images/logo/logo.png" alt="LOGO" class="logo__img">
+            </a>
+            <p class="copyright">© Терентьев Кирилл. Все права защищены 2022</p>
+         </div>
+      </div>
+   </footer>
+   <script src="assets/js/main.js"></script>
+</body>
+</html>
