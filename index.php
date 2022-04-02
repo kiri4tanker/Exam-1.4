c6275f301d2e56b39ba0605fe3bfd8b630a5e1de
Kirill Terentiev <kiri4tankerofficial@gmail.com>
2022-04-02 18:46:38 +0200

    fix and img-joke complete

--- index.php
+++ index.php
@@ -5,15 +5,15 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Сделаем лучше вместе!</title>
-   <link rel="stylesheet" href="asstes/css/main.css">
-   <link rel="stylesheet" href="asstes/css/media.css">
+   <link rel="stylesheet" href="assets/css/main.css">
+   <link rel="stylesheet" href="assets/css/media.css">
 </head>
 <body>
    <header class="header">
       <div class="container">
          <div class="header__content">
             <a href="index.php" class="logo">
-               <img src="assets/images/logo/logo.png" alt="LOGO" class="logo__img">
+               <img src="assets/images/logo/logo.svg" alt="LOGO">
             </a>
             <a href="login.php" class="btn">Вход</a>
          </div>
@@ -23,7 +23,7 @@
       <section class="section">
          <div class="container">
             <div class="section__heading">
-               <h1 class="sectuib__title">Городской портал - Сделаем лучше вместе!</h1>
+               <h1 class="section__title">Городской портал - Сделаем лучше вместе!</h1>
             </div>
             <div class="section__content">
                <!-- Счётчик -->
@@ -34,8 +34,8 @@
                <div class="applications">
                   <!-- 1 заявка -->
                   <div class="application">
-                     <img src="" alt="after" class="application__img img_after">
-                     <img src="" alt="before" class="application__img img_before">
+                     <img src="assets/images/applications/after/car.jpg" alt="after" class="application__img application__img_after">
+                     <img src="assets/images/applications/before/car-before.jpg" alt="before" class="application__img application__img_before">
                      <p class="application__name"></p>
                      <div class="inline">
                         <p class="application__category"></p>
@@ -44,8 +44,8 @@
                   </div>
                   <!-- 2 заявка -->
                   <div class="application">
-                     <img src="" alt="after" class="application__img img_after">
-                     <img src="" alt="before" class="application__img img_before">
+                     <img src="assets/images/applications/after/car.jpg" alt="after" class="application__img application__img_after">
+                     <img src="assets/images/applications/before/car-before.jpg" alt="before" class="application__img application__img_before">
                      <p class="application__name"></p>
                      <div class="inline">
                         <p class="application__category"></p>
@@ -54,8 +54,8 @@
                   </div>
                   <!-- 3 заявка -->
                   <div class="application">
-                     <img src="" alt="after" class="application__img img_after">
-                     <img src="" alt="before" class="application__img img_before">
+                     <img src="assets/images/applications/after/car.jpg" alt="after" class="application__img application__img_after">
+                     <img src="assets/images/applications/before/car-before.jpg" alt="before" class="application__img application__img_before">
                      <p class="application__name"></p>
                      <div class="inline">
                         <p class="application__category"></p>
@@ -64,8 +64,8 @@
                   </div>
                   <!-- 4 заявка -->
                   <div class="application">
-                     <img src="" alt="after" class="application__img img_after">
-                     <img src="" alt="before" class="application__img img_before">
+                     <img src="assets/images/applications/after/car.jpg" alt="after" class="application__img application__img_after">
+                     <img src="assets/images/applications/before/car-before.jpg" alt="before" class="application__img application__img_before">
                      <p class="application__name"></p>
                      <div class="inline">
                         <p class="application__category"></p>
@@ -81,7 +81,7 @@
       <div class="container">
          <div class="footer__content">
             <a href="index.php" class="logo">
-               <img src="assets/images/logo/logo.png" alt="LOGO" class="logo__img">
+               <img src="assets/images/logo/logo__footer.svg" alt="LOGO">
             </a>
             <p class="copyright">© Терентьев Кирилл. Все права защищены 2022</p>
          </div>
--- register.php
+++ register.php
@@ -8,15 +8,15 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Авторизация</title>
-   <link rel="stylesheet" href="asstes/css/main.css">
-   <link rel="stylesheet" href="asstes/css/media.css">
+   <link rel="stylesheet" href="assets/css/main.css">
+   <link rel="stylesheet" href="assets/css/media.css">
 </head>
 <body>
    <header class="header">
       <div class="container">
          <div class="header__content">
             <a href="index.php" class="logo">
-               <img src="assets/images/logo/logo.png" alt="LOGO" class="logo__img">
+               <img src="assets/images/logo/logo.svg" alt="LOGO">
             </a>
             <a href="login.php" class="btn">Главная</a>
          </div>
@@ -55,7 +55,7 @@
       <div class="container">
          <div class="footer__content">
             <a href="index.php" class="logo">
-               <img src="assets/images/logo/logo.png" alt="LOGO" class="logo__img">
+               <img src="assets/images/logo/logo__footer.svg" alt="LOGO">
             </a>
             <p class="copyright">© Терентьев Кирилл. Все права защищены 2022</p>
          </div>
